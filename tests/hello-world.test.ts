af4ba7477c669f38cadcedf58da62fcf44d300c8
talhasch <talhabugrabulut@gmail.com>
2024-10-25 15:29:42 +0300

    remote hello-world tests

--- tests/hello-world.test.ts
+++ tests/hello-world.test.ts
@@ -1,28 +1,7 @@
-
-import { beforeEach, describe, expect, it } from "vitest";
-import { deployGateway, getSigners } from "./util";
-import { bufferCVFromString, stringAsciiCV, uintCV } from "@stacks/transactions";
-
-const accounts = simnet.getAccounts();
-const address1 = accounts.get("wallet_1")!;
-
+import { describe, it } from "vitest";
 
 describe("hello-world tests", () => {
-  beforeEach(() => {
-    deployGateway(getSigners(0, 10, 1, 10, "1"));
-  });
-
-  it("send and execute", () => {
-    const destinationChain = 'Destination';
-    const destinationAddress = '0x123abc';
-    const payload = accounts.get("wallet_3")!;
+  it("placeholder", () => {
     
-    const { result } = simnet.callPublicFn("hello-world", "set-remote-value", [stringAsciiCV(destinationChain), stringAsciiCV(destinationAddress), bufferCVFromString(payload), uintCV(1_00000)], address1);
-    console.log(result)
   });
-
-  // it("shows an example", () => {
-  //   const { result } = simnet.callReadOnlyFn("counter", "get-counter", [], address1);
-  //   expect(result).toBeUint(0);
-  // });
 });
